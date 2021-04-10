f9c8177425c21c599b240ea3ba555d664d88e958
Shazzad Hossen <54257679+Shazzad-Hossen@users.noreply.github.com>
2021-04-10 23:00:02 +0600

    Update and rename NoteBook 1.py to Login_system_In_C_Language.c

---
+++ Login_system_In_C_Language.c
@@ -0,0 +1,49 @@
+# NoteBook 1
+# SHAZZAD  HOSSEN
+# ID: 191-15-2420
+#_________________________________
+
+
+
+#include<stdio.h>
+int main()
+
+{
+
+    int pass1=1234,i,pass2;
+
+
+    for(i=1; i<=5; i++)
+    {
+        printf("\n\nPlease Enter Password: ");
+        scanf("%d",&pass2);
+
+        if(pass1==pass2)
+        {
+            
+            printf("Correct!");
+            return 0;
+
+        }
+
+        else
+        {
+            
+            if((5-i)==0)printf("\n\nIncorrect Password! No attempts left");
+            else
+            printf("\n\nIncorrect Password! %d attempts left",5-i);
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+    return 0;
+}
